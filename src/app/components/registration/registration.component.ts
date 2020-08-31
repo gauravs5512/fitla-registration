bf151ab07d2af6ea38c3b4ddc997203eb53a6a02
sudhirpanier <sudhir.panier@gmail.com>
2020-09-01 00:01:15 +0800

    fix button submission and final data check

--- src/app/components/registration/registration.component.ts
+++ src/app/components/registration/registration.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { RegistrationService } from '../../services/registration.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Constant } from '../../constant2';
+import { ThrowStmt } from '@angular/compiler';
 
 @Component({
   selector: 'app-registration',
@@ -20,6 +21,7 @@ export class RegistrationComponent implements OnInit {
   public gender: string;
   public weight: string;
   public goal: string;
+  public isFormValid: boolean = false;
 
   constructor(private regService: RegistrationService, private snackBar: MatSnackBar) { }
 
@@ -57,34 +59,50 @@ export class RegistrationComponent implements OnInit {
     this.weight = reg.controls.weight.value;
     this.goal = reg.controls.goal.value;
 
-    let payload = { 
-      name: this.name,
-      password: this.password,
-      mobile: this.mobile,
-      weight: this.weight,
-      gender: this.gender,
-      goal: this.goal,
-      question: this.question,
-      andswer: this.answer,
-      email: this.email
-    };
-    console.log(payload);
-
-    Constant.loader = true;
-    this.regService.register(payload).subscribe(res => {
-      if(res && res['error']) {
-        alert(res['error'])
-      } else if(res['mobile']) {
-        alert('Registration Success');
-      }
-      Constant.loader = false;
-    }, err => {
-      alert('unknown error occured');
-      console.log(err);
-      Constant.loader = false;
-    });
-
-    this.onPage = 'thank-you';
+    if (
+      this.name !== undefined && 
+      this.password !== undefined && 
+      this.email !==undefined && 
+      this.mobile !== undefined &&
+      this.question !== undefined &&
+      this.answer !== undefined &&
+      this.gender !== undefined &&
+      this.weight !== undefined &&
+      this.goal !== undefined
+    ) {
+      let payload = { 
+        name: this.name,
+        password: this.password,
+        mobile: this.mobile,
+        weight: this.weight,
+        gender: this.gender,
+        goal: this.goal,
+        question: this.question,
+        andswer: this.answer,
+        email: this.email
+      };
+      console.log(payload);
+  
+      Constant.loader = true;
+      this.regService.register(payload).subscribe(res => {
+        if(res && res['error']) {
+          alert(res['error'])
+        } else if(res['mobile']) {
+          alert('Registration Success');
+        }
+        Constant.loader = false;
+      }, err => {
+        alert('unknown error occured');
+        console.log(err);
+        Constant.loader = false;
+      });
+  
+      this.onPage = 'thank-you';
+    } else {
+      this.snackBar.open("Please fill in all required fields before submitting", "⚠️", {
+        duration: 5000,
+      });
+    }
   }
 
 }
